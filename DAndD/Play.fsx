8c88d87b346804afc471991248599a76842e4a5b
Andrew Jones <ascjones@gmail.com>
2014-10-05 17:59:32 +0100

    implemented buildGrid function

--- DAndD/Play.fsx
+++ DAndD/Play.fsx
@@ -1,7 +1,46 @@
 ﻿#load "Model.fs"
 
 open Model
+open System
 
+let grid = @"
+########
+#      #    
+#####  #
+#      #
+#   #  #
+#####  #
+#      #
+########
+"
+
+let smallGrid = @"
+# b
+bgs
+"
+
+let buildGrid (gridAscii : string) = 
+    let cells,_ = 
+        gridAscii.Trim().ToCharArray()
+        |> Seq.fold (fun (cells,nextCoord) ch ->
+
+            if ch = '\010' then
+                let coord = { nextCoord with X = 0; Y = nextCoord.Y + 1 }
+                cells,coord
+            else
+                let cellState = 
+                    match ch with
+                    | '#' -> Blocked
+                    | ' ' -> Empty
+                    | 'b' -> ContainsItem(Bone)
+                    | 'g' -> ContainsItem(GoldCoin)
+                    | 's' -> ContainsItem(Scroll)
+                    | x -> failwithf "Unsupported character %A" x
+        
+                let coord = { nextCoord with X = nextCoord.X + 1; Y = nextCoord.Y }
+                let cell = { State = cellState; Coord = nextCoord }
+                cell::cells,coord ) ([],{ X = 0; Y = 0 })
+    cells
 
 let mutable player = { Id = "Andrew"; Coords = { X = 3; Y = 3 }; Orientation = Orientation.North }
 let game = { Players = [player]; Grid = { Width = 10; Height = 10} }
